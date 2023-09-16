89c23a825042c833715d732a21be4ac5e084c0f0
BigfootACA <bigfoot@classfun.cn>
2023-09-16 15:42:06 +0800

    efi-wrapper: main.c: add custom root

--- efi-wrapper/main.c
+++ efi-wrapper/main.c
@@ -7,6 +7,7 @@
 #include"logger.h"
 #include"efi/efi_file.h"
 #include"efi/efi_context.h"
+#include"efi/efi_wrapper.h"
 #include"efi/efi_template.h"
 #include"efi/protocols/graphics_output.h"
 
@@ -18,6 +19,7 @@ static int usage(){
 	puts(
 		"Usage: efi-wrapper [-o <FILE>] EFI...\n"
 		"Options:\n"
+		"\t-r, --root <ROOT>      Map path to UEFI filesystem\n"
 		"\t-o, --output <FILE>    Write log to file\n"
 		"\t-u, --skip-func <FUNC> Skip log print by func\n"
 		"\t-f, --skip-file <FILE> Skip log print by file\n"
@@ -41,14 +43,41 @@ void restore_terminal(){
 	winch_hand(SIGWINCH);
 }
 
+static void add_fs(efi_run_context*ctx,list*root){
+	list*l;
+	efi_status st;
+	efi_handle hand;
+	efi_run_context*cur;
+	if((l=list_first(root)))do{
+		bool writable=false;
+		const char*str;
+		LIST_DATA_DECLARE(path,l,const char*);
+		if(path[0]=='R'&&path[2]==':')switch(path[1]){
+			case 'O':writable=false,path+=3;break;
+			case 'W':writable=true,path+=3;break;
+			default:fprintf(stderr,"invalid path %s\n",path);continue;
+		}
+		str=writable?"writable":"read-only";
+		xlog(LOG_INFO,"add %s file system %s",str,path);
+		cur=efi_current_ctx,efi_current_ctx=ctx;
+		st=efi_new_simple_file_system_from_posix_path(path,writable,&hand,NULL);
+		if(efi_current_ctx==ctx)efi_current_ctx=cur;
+		if(efi_error(st))fprintf(
+			stderr,"add %s file system %s failed: %s\n",
+			str,path,efi_status_to_string(st)
+		);
+	}while((l=l->next));
+}
 
 int main(int argc,char**argv){
 	int o;
 	efi_file*f=NULL;
+	list*root=NULL;
 	efi_run_context*ctx=NULL;
 	efi_service*svc;
 	bool permissive=false;
 	static const struct option lo[]={
+		{"root",       required_argument, NULL, 'r'},
 		{"output",     required_argument, NULL, 'o'},
 		{"skip-func",  required_argument, NULL, 'u'},
 		{"skip-file",  required_argument, NULL, 'f'},
@@ -63,7 +92,7 @@ int main(int argc,char**argv){
 	xlog_set_verbose(false);
 	xlog_set_print_call_stack(false);
 	xlog_set_type(LOG_CALL,false);
-	while((o=getopt_long(argc,argv,"o:u:f:vschbp",lo,NULL))>0)switch(o){
+	while((o=getopt_long(argc,argv,"r:o:u:f:vschbp",lo,NULL))>0)switch(o){
 		case 'o':
 			if(xlog_open(optarg)<0)
 				fprintf(stderr,"open %s failed: %m\n",optarg);
@@ -72,6 +101,10 @@ int main(int argc,char**argv){
 			if((svc=efi_service_lookup_service(
 				&gEfiWrapperSDL2GraphicsOutputProtocolGuid
 			)))svc->disabled=false;
+		break;
+		case 'r':
+			if(!list_search_case_string(root,optarg))
+				list_obj_add_new_strdup(NULL,&root,optarg);
 		break;
 		case 'p':permissive=true;break;
 		case 'u':xlog_add_skip_func(optarg);break;
@@ -94,7 +127,9 @@ int main(int argc,char**argv){
 			need_restore=true;
 		winch_hand(SIGWINCH);
 	}
+	if(root)efi_service_config_unregister_by_name("UnixRoot");
 	if(!(ctx=efi_new_run_context()))return -1;
+	add_fs(ctx,root);
 	for(int i=optind;i<argc;i++){
 		if(!f&&!(f=efi_file_new(ctx)))continue;
 		f->permissive=permissive;
